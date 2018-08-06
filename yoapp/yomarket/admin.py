f3587c8665f48dd6d0eaa7eb272c015522267d24
Sergey Dusanyuk <s.dusanyuk@gmail.com>
2018-08-07 01:22:20 +0300

    updated offers, admin

--- yoapp/yomarket/admin.py
+++ yoapp/yomarket/admin.py
@@ -5,7 +5,7 @@ from .models import Shop, Offer
 class OfferAdmin(admin.ModelAdmin):
     list_display = ('title', 'shop', 'category', 'price', 'discount', 'discount_type', 'available', 'created')
     list_filter = ('shop', 'category', 'discount_type', 'available')
-    search_fields = ('title', 'shop', 'category')
+    search_fields = ('title', 'description')
     date_hierarchy = 'created'
     ordering = ['created', 'available']
 
