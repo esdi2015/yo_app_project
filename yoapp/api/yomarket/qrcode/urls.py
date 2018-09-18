21215359de487b782c1964dce0adb2242af17771
Vlad Honcharov <snork98@gmail.com>
2018-09-18 15:45:39 +0300

    added "/" in qrcodes URLs

--- yoapp/api/yomarket/qrcode/urls.py
+++ yoapp/api/yomarket/qrcode/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import  url
 from api.yomarket.qrcode.views import make_coupon,QRcouponsListView,QRcouponCheckView,QRcouponShortCheckView,QRcouponRedeemView,QRcouponShortRedeemView
 
 urlpatterns = [
-    url(r'^qr/redeem/(?P<uuid_id>.{12,50})$', QRcouponRedeemView.as_view(), name='redeem_coupon'),
-    url(r'^qr/redeem/(?P<id>.{0,12})$', QRcouponShortRedeemView.as_view(), name='short_redeem_coupon'),
+    url(r'^qr/redeem/(?P<uuid_id>.{12,50})/$', QRcouponRedeemView.as_view(), name='redeem_coupon'),
+    url(r'^qr/redeem/(?P<id>.{0,12})/$', QRcouponShortRedeemView.as_view(), name='short_redeem_coupon'),
 
-    url(r'^qr/check/(?P<uuid_id>.{12,50})$', QRcouponCheckView.as_view(), name='check_coupon'),
-    url(r'^qr/check/(?P<id>.{0,12})$', QRcouponShortCheckView.as_view(), name='short_check_coupon'),
+    url(r'^qr/check/(?P<uuid_id>.{12,50})/$', QRcouponCheckView.as_view(), name='check_coupon'),
+    url(r'^qr/check/(?P<id>.{0,12})/$', QRcouponShortCheckView.as_view(), name='short_check_coupon'),
 
     url(r'^qr/list-coupons/',QRcouponsListView.as_view(),name='list_coupons'),
     url(r'^qr/make-coupon/', make_coupon, name='make_coupon'),
