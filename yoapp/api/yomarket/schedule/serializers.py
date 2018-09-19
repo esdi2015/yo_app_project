52aac39d7cbce0c191a4a2ffdc575af5d8f42db8
Sergey Dusanyuk <s.dusanyuk@gmail.com>
2018-09-19 15:43:47 +0300

    adding schedule (2)

--- yoapp/api/yomarket/schedule/serializers.py
+++ yoapp/api/yomarket/schedule/serializers.py
@@ -6,16 +6,23 @@ ScheduleModel = apps.get_model('yomarket', 'Schedule')
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
-    # customer = serializers.StringRelatedField()
-    # customer_id = serializers.IntegerField(allow_null=False)
-    # manager = serializers.StringRelatedField()
-    # manager_id = serializers.IntegerField(allow_null=False)
-    # offer = serializers.StringRelatedField()
-    # offer_id = serializers.IntegerField(allow_null=False)
-    # points = serializers.IntegerField(allow_null=False, default=0, required=False)
+    mon_open = serializers.TimeField(format='%H:%M', )  #input_formats='%I:%M %p'
+    mon_close = serializers.TimeField(format='%H:%M', )
+    tue_open = serializers.TimeField(format='%H:%M', )
+    tue_close = serializers.TimeField(format='%H:%M', )
+    wed_open = serializers.TimeField(format='%H:%M', )
+    wed_close = serializers.TimeField(format='%H:%M', )
+    thu_open = serializers.TimeField(format='%H:%M', )
+    thu_close = serializers.TimeField(format='%H:%M', )
+    fri_open = serializers.TimeField(format='%H:%M', )
+    fri_close = serializers.TimeField(format='%H:%M', )
+    sat_open = serializers.TimeField(format='%H:%M', )
+    sat_close = serializers.TimeField(format='%H:%M', )
+    sun_open = serializers.TimeField(format='%H:%M', )
+    sun_close = serializers.TimeField(format='%H:%M', )
 
     class Meta:
         model = ScheduleModel
-        fields = ('id', 'title', 'mon_open', 'mon_close', 'tue_open', 'tue_close',
+        fields = ('id', 'title', 'shop', 'mon_open', 'mon_close', 'tue_open', 'tue_close',
                   'wed_open', 'wed_close', 'thu_open', 'thu_close', 'fri_open', 'fri_close',
                   'sat_open', 'sat_close', 'sun_open', 'sun_close')
