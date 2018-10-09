356b10a95e93805abeeae17d26a2943c5732025c
Vlad Honcharov <snork98@gmail.com>
2018-10-09 11:57:32 +0300

    fix targeting

--- yoapp/api/targeting/serializers.py
+++ yoapp/api/targeting/serializers.py
@@ -8,13 +8,23 @@ class UserPreferencesTableSerializer(serializers.ModelSerializer):
         model = UserPreferencesTable
         fields = ('data','user','object_type','object_id','created')
 
+    def create(self, validated_data):
+        instance = UserPreferencesTable(**validated_data, user=self.context['request'].user)
+        instance.save()
+
+        return instance
 
 
 class UserDataSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserDataTable
-        fields = ('birth_date','checkbox1','checkbox2')
+        fields = ('user','age','gender','checkbox1','checkbox2')
 
 
 
+    def create(self,validated_data):
+          instance= UserDataTable(**validated_data,user=self.context['request'].user)
+          instance.save()
+
+          return instance
--- yoapp/targeting/models.py
+++ yoapp/targeting/models.py
@@ -5,6 +5,12 @@ from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
 
+GENDERS = (
+    ('male', 'Male'),
+    ('female', 'Female')
+)
+
+
 OBJECT_TYPES = (
     ('OFFER', 'Offer'),
     ('CATEGORY', 'Category')
@@ -13,7 +19,7 @@ OBJECT_TYPES = (
 class UserPreferencesTable(models.Model):
     object_type = models.CharField(max_length=50, choices=OBJECT_TYPES, default=OBJECT_TYPES[1][1])
     object_id = models.IntegerField(default=0)
-    user = models.ForeignKey(UserModel,on_delete=models.CASCADE)
+    user = models.ForeignKey(UserModel,on_delete=models.CASCADE,null=True,blank=True)
     data = models.BooleanField()
     created = models.DateTimeField(auto_now_add=True)
 
@@ -21,7 +27,9 @@ class UserPreferencesTable(models.Model):
 
 
 class UserDataTable(models.Model):
-    birth_date = models.DateTimeField()
+    user = models.ForeignKey(UserModel,on_delete=models.CASCADE,null=True,blank=True)
+    age = models.IntegerField(null=True,default=0)
+    gender = models.CharField(max_length=15,choices=GENDERS,default=0,null=True)
     checkbox1 = models.BooleanField(default=False)
     checkbox2 = models.BooleanField(default=False)
 
